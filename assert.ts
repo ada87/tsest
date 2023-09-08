fa66f4cc09ac3d8c853fe13e18211ee6c38cb7ec
向达 <xiangda@vrvmail.com.cn>
2023-09-08 09:50:29 +0800

    ADD FUNCTIONS

--- assert.ts
+++ assert.ts
@@ -4,14 +4,31 @@ import assert from 'node:assert';
  *  https://nodejs.org/api/assert.html
 */
 
-const params = (item: Array<any>): [Array<any>, any] => {
-    return [item.slice(0, item.length - 1), item[item.length - 1]]
-}
+// const params = (item: Array<any>): [Array<any>, any] => {
+//     return [item.slice(0, item.length - 1), item[item.length - 1]]
+// }
+
+
+type FunctionArguments = any[];
+type FunctionReturn = any;
+type ResultCheckFunction<T> = (result: T) => boolean;
+
+/**
+ * Item Tuple : [ 0:args, 1:result ];
+ * args   :    Arguments pass by test funciton
+ * result :    Result of Function
+*/
+export type RecordByResult = [FunctionArguments, FunctionReturn];
+export type RecordByCheckFunction<T = any> = [FunctionArguments, ResultCheckFunction<T>];
+export type DataSetArguments = FunctionArguments[];
+export type DataSetWithResult = RecordByResult[];
+export type DataSetWithChecker<T = any> = RecordByCheckFunction<T>[];
+
 
 // Batch equal
-export const equal = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const equal = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item
         const actual = fn.apply(ctx, args);
         assert.equal(actual, expected);
     }
@@ -19,9 +36,9 @@ export const equal = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null)
 
 
 // Batch notEqual
-export const notEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const notEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item
         const actual = fn.apply(ctx, args);
         assert.notEqual(actual, expected);
     }
@@ -30,54 +47,54 @@ export const notEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = nu
 
 
 // Batch deepEqual
-export const deepEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const deepEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item
         const actual = fn.apply(ctx, args);
         assert.deepEqual(actual, expected);
     }
 }
 
 // Batch notDeepEqual
-export const notDeepEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const notDeepEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item
         const actual = fn.apply(ctx, args);
         assert.notDeepEqual(actual, expected);
     }
 }
 
 // Batch strictEqual
-export const strictEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const strictEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item
         const actual = fn.apply(ctx, args);
         assert.strictEqual(actual, expected);
     }
 }
 
 // Batch notStrictEqual
-export const notStrictEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const notStrictEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item
         const actual = fn.apply(ctx, args);
         assert.notStrictEqual(actual, expected);
     }
 }
 
 // Batch deepStrictEqual
-export const deepStrictEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const deepStrictEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item;
         const actual = fn.apply(ctx, args);
         assert.deepStrictEqual(actual, expected);
     }
 }
 
 // Batch notDeepStrictEqual
-export const notDeepStrictEqual = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const notDeepStrictEqual = (fn: Function, dataSet: DataSetWithResult, ctx: any = null) => {
     for (let item of dataSet) {
-        const [args, expected] = params(item);
+        const [args, expected] = item;
         const actual = fn.apply(ctx, args);
         assert.notDeepStrictEqual(actual, expected);
     }
@@ -97,18 +114,36 @@ export const match = (reg: RegExp, dataSet: Array<[string, boolean]>) => {
 }
 
 
-export const isTrue = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const isTrue = (fn: Function, dataSet: DataSetArguments, ctx: any = null) => {
     for (let item of dataSet) {
         const actual = fn.apply(item);
         assert.equal(actual, true);
     }
 }
 
-export const isFalse = (fn: Function, dataSet: Array<Array<any>>, ctx: any = null) => {
+export const isFalse = (fn: Function, dataSet: DataSetArguments, ctx: any = null) => {
     for (let item of dataSet) {
         const actual = fn.apply(ctx, item);
         assert.equal(actual, false);
     }
 }
 
+export const doesNotThrow = (fn: Function, dataSet: DataSetArguments, ctx: any = null) => {
+    for (let args of dataSet) {
+        assert.doesNotThrow(fn.apply(ctx, args))
+    }
+}
 
+export const doesNotReject = (fn: Function, dataSet: DataSetArguments, ctx: any = null) => {
+    for (let args of dataSet) {
+        assert.doesNotReject(fn.apply(ctx, args))
+    }
+}
+
+export const byCheckFunction = <T = any>(fn: (...args: any[]) => T, dataSet: DataSetWithChecker<T>, ctx: any = null) => {
+    for (let record of dataSet) {
+        const [args, checker] = record;
+        const returnValue = fn.apply(ctx, args);
+        assert.equal(checker(returnValue), true);
+    }
+}
--- test.ts
+++ test.ts
@@ -1,24 +1,28 @@
 import { test } from 'node:test';
+import {
+    equal, notEqual, deepEqual, notDeepEqual, deepStrictEqual, notDeepStrictEqual, strictEqual, notStrictEqual,
+    isTrue, isFalse, doesNotThrow, doesNotReject,
+    byCheckFunction,
+} from './assert';
 
-import { equal, notEqual, deepEqual, notDeepEqual, deepStrictEqual, notDeepStrictEqual, strictEqual, notStrictEqual, isTrue, isFalse } from './assert';
+import type { DataSetArguments, DataSetWithResult, DataSetWithChecker } from './assert'
 
-export { test };
+export const testEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => equal(fn, dataSet, ctx));
+export const testnotEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => notEqual(fn, dataSet, ctx));
 
+export const testStrictEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => strictEqual(fn, dataSet, ctx));
+export const testNotStrictEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => notStrictEqual(fn, dataSet, ctx));
 
+export const testDeepEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => deepEqual(fn, dataSet, ctx));
+export const testNotDeepEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => notDeepEqual(fn, dataSet, ctx));
 
-export const testEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => equal(fn, dataSet, ctx));
-export const testnotEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => notEqual(fn, dataSet, ctx));
+export const testDeepStrictEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => deepStrictEqual(fn, dataSet, ctx));
+export const testNotDeepStrictEqual = (fn: Function, dataSet: DataSetWithResult) => test(`Test [${fn.name}]`, (ctx) => notDeepStrictEqual(fn, dataSet, ctx));
 
-export const testStrictEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => strictEqual(fn, dataSet, ctx));
-export const testNotStrictEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => notStrictEqual(fn, dataSet, ctx));
+export const testIsTure = (fn: Function, dataSet: DataSetArguments) => test(`Test [${fn.name}]`, (ctx) => isTrue(fn, dataSet, ctx));
+export const testIsFalse = (fn: Function, dataSet: DataSetArguments) => test(`Test [${fn.name}]`, (ctx) => isFalse(fn, dataSet, ctx));
 
-export const testDeepEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => deepEqual(fn, dataSet, ctx));
-export const testNotDeepEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => notDeepEqual(fn, dataSet, ctx));
+export const testDoesNotThrow = (fn: Function, dataSet: DataSetArguments) => test(`Test [${fn.name}]`, (ctx) => doesNotThrow(fn, dataSet, ctx));
+export const testDoesNotReject = (fn: Function, dataSet: DataSetArguments) => test(`Test [${fn.name}]`, (ctx) => doesNotReject(fn, dataSet, ctx));
 
-export const testDeepStrictEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => deepStrictEqual(fn, dataSet, ctx));
-export const testNotDeepStrictEqual = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => notDeepStrictEqual(fn, dataSet, ctx));
-
-export const testIsTure = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => isTrue(fn, dataSet, ctx));
-export const testIsFalse = (fn: Function, dataSet: Array<Array<any>>) => test(`Test [${fn.name}]`, (ctx) => isFalse(fn, dataSet, ctx));
-
-
+export const testByCheckFunction = <T = any>(fn: (...args: any[]) => T, dataSet: DataSetWithChecker) => test(`Test [${fn.name}]`, (ctx) => byCheckFunction(fn, dataSet, ctx));
